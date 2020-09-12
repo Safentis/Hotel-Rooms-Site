108b29eadc81325f1c5f31dcd27e94bc324e2a68
Safentis <fryazinovanton@gmail.com>
2020-09-13 01:46:24 +0300

    'create_footer_structure'

--- src/assets/scripts/dropdown/dropdown.js
+++ src/assets/scripts/dropdown/dropdown.js
@@ -1,27 +1,24 @@
 function dropdownMenu() {
     
-    const dropdownOn = ({target}) => {
+    const dropdownToggle = ({target}) => {
         if (!target.dataset.dropdown) return;
-        const listIltems = target.nextElementSibling;
 
-        if (listIltems.classList.contains('dropdown_hide')) {
-            listIltems.classList.remove('dropdown_hide');
-        } else {
-            listIltems.classList.add('dropdown_hide');
-        }
+        const listIltems = target.nextElementSibling;
+        listIltems.classList.toggle('dropdown_hide');
     }
     
-    const dropdownValue = ({target}) => {
-        if (!target.dataset.value) return;
-        const itemValue = target.dataset.value;
-        const input = target.closest('ul');
+    const dropdownSetValue = ({target}) => {
+        const listValue = target.dataset.value; 
         
-        input.classList.add('dropdown_hide');
-        input.previousElementSibling.value = itemValue;
+        if (!listValue) return;
+        const itemValue = listValue;
+        const list = target.closest('ul');
+        
+        list.previousElementSibling.value = itemValue;
     }
 
-    document.addEventListener('click', dropdownOn);
-    document.addEventListener('click', dropdownValue);
+    document.addEventListener('click', dropdownToggle);
+    document.addEventListener('click', dropdownSetValue);
 }
 
 dropdownMenu();
